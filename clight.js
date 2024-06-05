8dc796818e924c0e36d6271fdc450c5a665b3dc3
Cybenv <cybenv@duck.com>
2024-06-05 12:17:48 +0300

    Refactor previous script to improve performance

--- clight.js
+++ clight.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Codecademy: Light Mode
 // @namespace    https://github.com/cybenv
-// @version      0.6
+// @version      1.0
 // @description  Codecademy Enhanced Light Theme with Customizations
 // @author       Seva
 // @match        https://www.codecademy.com/*
@@ -9,111 +9,50 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-
     const css = `
-        header.gamut-7fo7a4-Box {
-            background-color: #fffcf9;
-            border-bottom: 0.9px solid black;
-        }
- 
-        header.gamut-7fo7a4-Box * {
-            color: #3d3a39;
+        /* Верхняя панель */
+        .gamut-17zcbnm-Box header {
+            background-color: #FFFFFF; /* Цвет фона верхней панели */
         }
 
-        header.gamut-7fo7a4-Box :hover {
-        color: #ff8668;
+        .gamut-jmy50 {
+            color: black; /* Цвет логотипа */
         }
 
-        div.styles_footer__uX91v {
-            background-color: #fffcf9;
+        .gamut-gi2olp-AnchorBase {
+            color: black; /* Цвет 'My Home' */
         }
 
-        footer {
-            background-color: #fffcf9;
-            border-top: 0.9px solid black;
+        .gamut-aqft1r-ResetElement-AnchorBase {
+            color: black; /* Цвет 'Syllabus' */
         }
 
-        .gamut-1541y8v-ResetElementAnchor-createButtonComponent, 
-        button#discovery-next, button#discovery-back {
-            color: #3d3a39;
+        .gamut-11n1gpx-ResetElement-AnchorBase {
+            color: black; /* Цвет 'Get Unstuck' */
         }
 
-        button#discovery-next {
-            background-color: #ff8668;
-            border-color: #ff8668;
+        .gamut-1cgj4ay-ResetElement-AnchorBase {
+            color: black; /* Цвет 'Tools' */
         }
 
-        button#discovery-back {
-            border-color: #ff8668;
-        }
-
-        .styles_sectionRight__sgWaF .gamut-1541y8v-ResetElementAnchor-createButtonComponent {
-            background-color: #ff8668;
-            border: #ff8668;
-            color: #3d3a39;
-        }
-
-        .styles_sectionRight__sgWaF a.gamut-1541y8v-ResetElementAnchor-createButtonComponent {
-            color: #3d3a39;
-            background-color: #ff8668;
-        }
-        /* Main страницы */
-        .gamut-1mv8uod :not(.gamut-1ag67m1-FlexBox):not(.gamut-1ag67m1-FlexBox *):not(.gamut-80zlcx-Box):not(.gamut-80zlcx-Box *) {
-            background-color: #FFF1E6;
-        }
-        /* Контент окно курса */
-        .gamut-1ag67m1-FlexBox {
-            background-color: #fffef9;
-            border: 0.9px solid black;
-            padding: 35px;
-            position: relative;
-            overflow: visible;
-            box-shadow: -10px 15px 0px 0px rgba(0, 0, 0, 0.15);
-        }
-
-        h1.gamut-oqq5yh-StyledText, 
-        span.styles_markdown__Y5HHD {
-            color: #3d3a39;
-        }
-
-        .gamut-93stl9-Box, 
-        .gamut-93stl9-Box * {
-            color: #2a2a29;
-        }
-
-        div.gamut-80zlcx-Box, 
-        div.gamut-80zlcx-Box *:not(.gamut-1gjic71-BadgeBase):not(.gamut-1gjic71-BadgeBase *) {
-            background-color: #fffef9;
-        }
-
-        div.gamut-80zlcx-Box {
-            border: 0.9px solid black;
-            color: #2a2a29;
-        }
-
-        .styles_p__TNq46 > code.styles_code__L5_fh {
-            display: inline-block;
-            padding: 0.02em 0.4em;
-            margin: 0;
-            font-size: 75%;
-            color: #2a2a29;
-            background-color: #dfd6f1;
-        }
-
-        div * > a.gamut-d5kubd-AnchorBase {
-            color: #444fb2;
+        .gamut-7fo7a4-Box :hover {
+            color: #ff8668; /* Цвет при наведении */
         }
     `;
 
     function addGlobalStyle(css) {
         const head = document.getElementsByTagName('head')[0];
         if (!head) {
+            console.error('No head element found in the document.');
             return;
         }
         const style = document.createElement('style');
         style.type = 'text/css';
-        style.innerHTML = css;
+        if (style.styleSheet) { // Для IE
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
         head.appendChild(style);
     }
 
