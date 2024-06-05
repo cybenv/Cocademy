56ee3b959ef964d9884e8a25e09e3b1ac57013ae
Cybenv <cybenv@duck.com>
2024-06-06 01:47:19 +0300

    Partial content update in main

--- clight.js
+++ clight.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Codecademy: Light Mode
 // @namespace    https://github.com/cybenv
-// @version      1.0
+// @version      1.1
 // @description  Codecademy Enhanced Light Theme with Customizations
 // @author       Seva
 // @match        https://www.codecademy.com/*
@@ -13,6 +13,7 @@
         /* Верхняя панель */
         .gamut-17zcbnm-Box header {
             background-color: #FFFFFF; /* Цвет фона верхней панели */
+            border-bottom: 1px solid black; /* Цвет нижней границы верхней панели */
         }
 
         .gamut-jmy50 {
@@ -40,9 +41,12 @@
         }
 
         /* Нижняя панель */
-
         div.styles_footer__uX91v {
             background-color: #FFFFFF; /* Цвет фона нижней панели */
+        }
+
+        footer {
+            border-top: 1px solid black;
         }
 
         button#discovery-back {
@@ -61,7 +65,57 @@
         }
 
         button#discovery-back :hover {
-            background-color: #ff8668; /* Цвет кнопки 'Back' при нажатии */
+            background-color: #ff8668; /* Цвет кнопки 'Back' при наведении */
+        }
+
+        /* Main страницы */
+        main :not(.gamut-1ag67m1-FlexBox):not(.gamut-1ag67m1-FlexBox *) {
+            background-color: #FFF1E6; /* Цвет фона для всего body */
+        }
+
+        .gamut-1ag67m1-FlexBox {
+            background-color: #fffef9; /* Устанавливает светло-кремовый цвет фона для основного контейнера контента */
+            border: 1px solid black; /* Добавляет тонкую черную рамку вокруг контейнера */
+            display: grid; /* Устанавливает тип отображения содержимого, чтобы оно занимало всю доступную ширину */
+            box-shadow: -10px 10px 0px 1px rgba(0, 0, 0, 0.15); /* Добавляет тень к контейнеру */
+            padding-left: 2rem; /* Добавляет отступ слева от контейнера */
+            padding-right: 2rem; /* Добавляет отступ справа от контейнера */
+            padding-top: 1.5rem; /* Добавляет отступ сверху от контейнера */
+            padding-bottom: 1.5rem; /* Добавляет отступ снизу от контейнера */
+        }
+
+        /* Начало изменений для черного цвета шрифта */
+        .styles_h2__X9hwU,
+        .styles_h3__VPpU9,
+        .styles_h4__t9RTs,
+        .styles_p__TNq46,
+        .styles_li__mfNCB,
+        .styles_blockquote__2o8ay,
+        .styles_details__ovHPh {
+            color: black;
+        }
+        /* Конец изменений для черного цвета шрифта */
+
+        @media (max-width: 425px) { /* Применяем только на экранах меньше 425px */
+            .gamut-1ag67m1-FlexBox {
+                display: block; /* Меняем display на block */
+                width: auto; /* Позволяем элементу принимать нужную ширину */
+            }
+        }
+
+        .styles_p__TNq46 > code.styles_code__L5_fh {
+            display: inline-block; /* Устанавливает тип отображения элемента code как inline-block */
+            padding: 0.02em 0.4em; /* Устанавливает внутренние отступы сверху и снизу, слева и справа */
+            margin: 0; /* Убирает внешние отступы элемента */
+            font-size: 75%; /* Устанавливает размер шрифта 75% от родительского */
+            color: #2a2a29; /* Устанавливает цвет текста */
+            background-color: #dfd6f1; /* Устанавливает фоновый цвет */
+            border: 1px solid black; /* Добавляет рамку */
+            border-radius: 2px; /* Добавляет радиус */
+        }
+
+        a.gamut-d5kubd-AnchorBase {
+            color: #444fb2; /* Меняет цвет якоря */
         }
     `;
 
